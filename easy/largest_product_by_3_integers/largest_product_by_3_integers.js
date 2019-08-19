e2c640e1201d4b6debce9096677869139d5a1ff4
Rubinder Singh <mail2rubinder@gmail.com>
2019-08-19 11:52:13 +1000

    Added solutions to 'Largest product by 3 integers' and 'Largest Sum of Adjacent Numbers' (#11)

---
+++ easy/largest_product_by_3_integers/largest_product_by_3_integers.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number[]} arr
+ * @returns {number}
+ */
+function largestProductBy3Integers(arr) {
+  if (arr.length < 3) {
+    throw 'Array needs to have at least 3 integers';
+  }
+
+  let a = arr[0];
+  let b = arr[1];
+  let c = arr[2];
+
+  for (let i = 3; i < arr.length; i++) {
+    let product = a * b * c;
+    let productA = arr[i] * b * c;
+    let productB = a * arr[i] * c;
+    let productC = a * b * arr[i];
+    let maxProduct = Math.max(productA, productB, productC);
+
+    if (maxProduct > product) {
+      if (maxProduct === productA) {
+        a = arr[i];
+      } else if (maxProduct === productB) {
+        b = arr[i];
+      } else if (maxProduct === productC) {
+        c = arr[i];
+      }
+    }
+  }
+  return a * b * c;
+}
+
+/* Test */
+console.log(largestProductBy3Integers([-10, -10, 5, 2])); // 500
+console.log(largestProductBy3Integers([5, 8, 9, 0])); // 360
