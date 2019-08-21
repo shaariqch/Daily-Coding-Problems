97bdfed4e8c978452fc981858c90f25fd11ef056
Rubinder Singh <mail2rubinder@gmail.com>
2019-08-21 14:10:31 +1000

    longestSubstringWithKDistinctChars JS Solution (#13)

--- hard/longest_substring_k_distinct_characters/README.md
+++ hard/longest_substring_k_distinct_characters/README.md
@@ -1,9 +1,5 @@
-# Longest Substring with ```k``` distinct characters
+# Longest Substring with `k` distinct characters
 
-Good morning! Here's your coding interview problem for today.
+Given an integer `k` and a string `s`, find the longest substring that contains at most `k` distinct characters.
 
-This problem was asked by Amazon.
-
-Given an integer ```k``` and a string ```s```, find the length of the longest substring that contains at most ```k``` distinct characters.
-
-For example, given ```s = "abcba"``` and ```k = 2```, the longest substring with ```k``` distinct characters is ```"bcb"```.
+For example, given `s = "abcba"` and `k = 2`, the longest substring with `k` distinct characters is `"bcb"`.
---
+++ hard/longest_substring_k_distinct_characters/longest_Substring_with_k_distinct_characters.js
@@ -0,0 +1,60 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @returns {string}
+ */
+function removeFirstDistinctChar(s, k) {
+  let currS = '';
+  let nDistinct = 0;
+  for (let i = s.length - 1; i >= 0; i--) {
+    if (currS.indexOf(s[i]) === -1) {
+      nDistinct++;
+      if (nDistinct <= k) {
+        currS += s[i];
+      } else {
+        return s.slice(i + 1);
+      }
+    }
+  }
+  return s;
+}
+
+/**
+ * @param {string} s
+ * @param {number} k
+ * @returns {string}
+ */
+function longestSubstringWithKDistinctChars(s, k) {
+  if (k <= 0) {
+    throw 'Number of distinct characters must greater than zero';
+  }
+
+  let nDistinct = 0;
+  let currS = '';
+  let lastS = '';
+  for (let i = 0; i < s.length; i++) {
+    if (currS.indexOf(s[i]) !== -1) {
+      currS += s[i];
+    } else {
+      nDistinct++;
+      if (nDistinct <= k) {
+        currS += s[i];
+      } else {
+        if (currS.length >= lastS.length) {
+          lastS = currS;
+        }
+        currS = removeFirstDistinctChar(currS, k - 1) + s[i];
+      }
+    }
+  }
+
+  return currS.length >= lastS.length ? currS : lastS;
+}
+
+/* Test */
+console.log(longestSubstringWithKDistinctChars('abcba', 2)); // bcb
+console.log(longestSubstringWithKDistinctChars('aaabcba', 2)); // aaab
+console.log(longestSubstringWithKDistinctChars('aaabcba', 3)); // aaabcba
+console.log(longestSubstringWithKDistinctChars('bbbbzaaabcba', 3)); // bbbbzaaab
+console.log(longestSubstringWithKDistinctChars('ab', 3)); // ab
+console.log(longestSubstringWithKDistinctChars('ab', 1)); // b
