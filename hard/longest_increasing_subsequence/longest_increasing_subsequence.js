12190796e83ba942a763d5fe8f0d549ef7f324b0
Rubinder25 <mail2rubinder@gmail.com>
2019-08-25 07:38:48 +1000

    in progress

--- hard/longest_increasing_subsequence/longest_increasing_subsequence.js
+++ hard/longest_increasing_subsequence/longest_increasing_subsequence.js
@@ -22,53 +22,65 @@ function longest_increasing_subsequence_Length(arr) {
  */
 function longest_increasing_subsequence_Array(arr) {
   const lis = Array(arr.length);
-  for (let i = 0; i < lis.length; i++) {
+  for (let i = 0; i < arr.length; i++) {
     lis[i] = Array();
-  }
-  lis[0].push(arr[0]);
-  debugger;
-  for (let i = 1; i < arr.length; i++) {
     lis[i].push(arr[i]);
+  }
 
-    let temp = [];
+  for (let i = 1; i < arr.length; i++) {
     for (let j = 0; j < i; j++) {
       if (arr[j] < arr[i] && lis[i].length <= lis[j].length + 1) {
-        debugger;
-        temp = [...lis[j], ...lis[i]];
-        debugger;
+        lis[i] = [...lis[j], arr[i]];
       }
     }
-    lis[i] = temp;
   }
-  console.log(lis);
 
-  return lis.reduce((acc, val) => );
+  return lis.reduce((acc, val) => (acc.length >= val.length ? acc : val));
 }
 
-console.log('1'+ longest_increasing_subsequence_Array([0, 1, 2]));
+console.log(
+  longest_increasing_subsequence_Array([
+    0,
+    8,
+    4,
+    12,
+    2,
+    10,
+    6,
+    14,
+    1,
+    9,
+    5,
+    13,
+    3,
+    11,
+    7,
+    15,
+  ]),
+);
 
 /* Test */
 const tests = [
   {
-    desc: "1",
+    desc: '1',
     args: [[0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15]],
-    res: 6
+    res: 6,
   },
   {
-    desc: "2",
+    desc: '2',
     args: [[1, 2, 3, 4, 5]],
-    res: 5
+    res: 5,
   },
   {
-    desc: "3",
+    desc: '3',
     args: [[1, 2, 0, 4, 5]],
-    res: 4
+    res: 4,
   },
   {
-    desc: "3",
+    desc: '3',
     args: [[1, 2, 0, -4, 5]],
-    res: 3
-  }
+    res: 3,
+  },
 ];
 
 // tests.forEach(test => {
