b628e1818b54f94974025ef4c82a3fa97ddaf1fb
Rubinder25 <rubinder.singh@nextgen.net>
2019-09-03 13:26:54 +1000

    test setup finished

--- tests/run_tests.js
+++ tests/run_tests.js
@@ -12,7 +12,12 @@ function fileScannerSync(args) {
     var stats = fs.statSync(fullPath);
     if (stats.isDirectory()) {
       if (args.recursive && !args.excludeList.includes(item)) {
-        fileScannerSync(args);
+        fileScannerSync({
+          dirPath: fullPath,
+          recursive: args.recursive,
+          excludeList: args.excludeList,
+          callback: args.callback,
+        });
       }
       args.callback(fullPath, true);
     } else {
@@ -21,20 +26,67 @@ function fileScannerSync(args) {
   });
 }
 
+function runTest(test, func) {
+  const nanoPerSec = 1e9;
+  const nanoPerMilli = 1000000;
+  const timeStart = process.hrtime();
+
+  const receivedResult = func(...test.args);
+
+  const timeStop = process.hrtime(timeStart);
+  const timeInNano = timeStop[0] * nanoPerSec + timeStop[1];
+  const timeInMilli = timeInNano / nanoPerMilli;
+
+  if (test.exp === receivedResult) {
+    process.stdout.write(
+      `Pass: ${test.desc} | ${test.args.join(', ')} => ${
+        test.exp
+      } in ${timeInMilli} ms\n`,
+    );
+  } else {
+    process.stdout.write(`\nFail: ${test.desc}\n`);
+    process.stdout.write(
+      `expected: ${test.exp} | received: ${receivedResult}\n\n`,
+    );
+  }
+
+  return timeInMilli;
+}
+
 fileScannerSync({
   dirPath: '.',
   recursive: true,
-  excludeList: ['tests'],
+  excludeList: ['tests', '.git'],
   callback: (fullPath, isDir) => {
     const tests = [];
+    let totalTimeTaken = 0;
     if (!isDir) {
       if (path.extname(fullPath) === '.js') {
-        const testPath = path.dirname(fullPath) + 'test.json';
-        if (!tests[testPath]) {
-          tests[testPath] = JSON.parse(fs.readFileSync(testPath, 'utf-8'));
-        }
+        const testPath = path.join(path.dirname(fullPath), 'tests.json');
 
-        
+        if (fs.existsSync(testPath)) {
+          const funcToBeTested = require(path.join('..' + path.sep, fullPath));
+
+          if (!tests[testPath]) {
+            tests[testPath] = JSON.parse(fs.readFileSync(testPath, 'utf-8'));
+          }
+
+          process.stdout.write(
+            `\nTest: ${path.basename(fullPath)} => ${funcToBeTested.name}\n\n`,
+          );
+
+          tests[testPath].tests.forEach((test) => {
+            totalTimeTaken += runTest(test, funcToBeTested);
+          });
+          process.stdout.write(
+            `\nTotal Time: ${totalTimeTaken}, Avg Time: ${totalTimeTaken /
+              tests[testPath].tests.length}\n`,
+          );
+
+          process.stdout.write(
+            '_______________________________________________________________________________\n',
+          );
+        }
       }
     }
   },
