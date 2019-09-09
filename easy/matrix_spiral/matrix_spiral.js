a711c6070e5957fb80ef0c6e544bdfc84b8c0d16
Rubinder25 <rubinder.singh@nextgen.net>
2019-09-09 12:24:53 +1000

    test.json file added

--- easy/matrix_spiral/matrix_spiral.js
+++ easy/matrix_spiral/matrix_spiral.js
@@ -43,12 +43,4 @@ function displayMatrixSpiral(mat) {
   console.log(spiral.join('\n'));
 }
 
-/* Test */
-const mat = [
-  [1, 2, 3, 4, 5],
-  [6, 7, 8, 9, 10],
-  [11, 12, 13, 14, 15],
-  [16, 17, 18, 19, 20],
-];
-
-displayMatrixSpiral(mat);
+module.exports = matrixSpiral;
