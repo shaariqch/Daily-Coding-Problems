9530bd1120ca63ac1dbf44270ab0f6665b73ee40
Rubinder25 <rubinder.singh@nextgen.net>
2019-09-09 11:48:43 +1000

    script updated

--- tests/run_tests.js
+++ tests/run_tests.js
@@ -1,92 +1,133 @@
 const fs = require('fs');
 const path = require('path');
+
 const testSpecified = process.argv[2];
 
-function fileScannerSync(args) {
-  if (!fs.statSync(args.dirPath).isDirectory()) {
-    args.callback(args.dirPath, false);
-    return;
-  }
-  var dirList = fs.readdirSync(args.dirPath);
-  dirList.forEach(function(item) {
-    var fullPath = path.join(args.dirPath, item);
-    var stats = fs.statSync(fullPath);
-    if (stats.isDirectory()) {
-      if (args.recursive && !args.excludeList.includes(item)) {
-        fileScannerSync({
-          dirPath: fullPath,
-          recursive: args.recursive,
-          excludeList: args.excludeList,
-          callback: args.callback,
-        });
-      }
-      args.callback(fullPath, true);
+let totalFilesTested = 0;
+let totalTestsPassed = 0;
+let totalTestsFailed = 0;
+
+function arrContains(list, val) {
+  let isContains = false;
+
+  list.forEach((ele) => {
+    let eleRegExp;
+
+    if (typeof ele === 'string') {
+      eleRegExp = new RegExp(`^${escapeRegExp(ele)}$`);
     } else {
-      args.callback(fullPath, false);
+      // type is RegExp
+      eleRegExp = ele;
+    }
+
+    if (val.match(eleRegExp)) {
+      isContains = true;
     }
   });
+
+  return isContains;
+}
+
+function isDir(dirPath) {
+  return fs.statSync(dirPath).isDirectory();
+}
+
+function fileScannerSync(args) {
+  if (fs.existsSync(args.path) && !arrContains(args.excludeList, path.basename(args.path))) {
+    if (!isDir(args.path)) {
+      args.callback(path.normalize(args.path), false);
+      return;
+    }
+
+    const dirList = fs.readdirSync(args.path);
+
+    dirList.forEach((item) => {
+      if (!arrContains(args.excludeList, path.basename(item))) {
+        const relativePath = path.join(args.path, item);
+
+        if (args.recursive) {
+          const argsCallback = Object.assign({}, args);
+          argsCallback.path = relativePath;
+          fileScannerSync(argsCallback);
+        } else {
+          args.callback(path.normalize(relativePath), isDir(relativePath));
+        }
+      }
+    });
+
+    args.callback(path.normalize(args.path), true);
+
+    return;
+  }
 }
 
 function runTest(test, func) {
+  const returnObj = {
+    timeTaken: 0,
+    hasPassed: false,
+    received: null,
+  };
+
   const nanoPerSec = 1e9;
   const nanoPerMilli = 1000000;
   const timeStart = process.hrtime();
 
-  const receivedResult = func(...test.args);
+  returnObj.received = func(...test.args);
 
   const timeStop = process.hrtime(timeStart);
   const timeInNano = timeStop[0] * nanoPerSec + timeStop[1];
   const timeInMilli = timeInNano / nanoPerMilli;
 
-  if (test.exp === receivedResult) {
-    process.stdout.write(
-      `Test: ${test.desc} \u2713 | ${test.args.join(', ')} => ${
-        test.exp
-      } in ${timeInMilli} ms\n`,
-    );
-  } else {
-    process.stdout.write(`\nTest: ${test.desc} \u2717\n`);
-    process.stdout.write(
-      `expected: ${test.exp} | received: ${receivedResult}\n\n`,
-    );
+  returnObj.timeTaken = timeInMilli;
+
+  if (test.exp === returnObj.received) {
+    returnObj.hasPassed = true;
   }
 
-  return timeInMilli;
+  return returnObj;
 }
 
-let totalFilesTested = 0;
-let totalTestsRan = 0;
-
 fileScannerSync({
-  dirPath: '.',
+  path: '.',
   recursive: true,
-  excludeList: ['tests', '.git'],
-  callback: (fullPath, isDir) => {
+  excludeList: [],
+  callback: (relativePath, isDir) => {
     const tests = [];
     let totalTimeTaken = 0;
 
     if (!isDir) {
       if (
-        path.extname(fullPath) === '.js' &&
-        (testSpecified === undefined || fullPath.indexOf(testSpecified) !== -1)
+        path.extname(relativePath) === '.js' &&
+        (testSpecified === undefined || relativePath.indexOf(testSpecified) !== -1)
       ) {
-        const testPath = path.join(path.dirname(fullPath), 'tests.json');
+        const testPath = path.join(path.dirname(relativePath), 'tests.json');
 
         if (fs.existsSync(testPath)) {
           totalFilesTested++;
-          const funcToBeTested = require(path.join('..' + path.sep, fullPath));
+          const funcToBeTested = require(path.join('..' + path.sep, relativePath));
 
           if (!tests[testPath]) {
             tests[testPath] = JSON.parse(fs.readFileSync(testPath, 'utf-8'));
           }
 
-          process.stdout.write(
-            `\nTest: ${path.basename(fullPath)} => ${funcToBeTested.name}\n\n`,
-          );
+          process.stdout.write(`\nTest: ${path.basename(relativePath)} => ${funcToBeTested.name}\n\n`);
 
           tests[testPath].tests.forEach((test) => {
-            totalTestsRan++;
-            totalTimeTaken += runTest(test, funcToBeTested);
+            const testResObj = runTest(test, funcToBeTested);
+            totalTimeTaken += testResObj.timeTaken;
+
+            if (testResObj.hasPassed) {
+              totalTestsPassed++;
+              process.stdout.write(
+                `Test: ${test.desc} \u2713 | ${test.args.join(', ')} => ${test.exp} in ${
+                  testResObj.timeTaken
+                } ms\n`,
+              );
+            } else {
+              totalTestsFailed++;
+              process.stdout.write(`\nTest: ${test.desc} \u2717 in ${testResObj.timeTaken} ms\n`);
+              process.stdout.write(`expected: ${test.exp} | received: ${testResObj.received}\n\n`);
+            }
           });
 
           process.stdout.write(
@@ -103,5 +144,7 @@ fileScannerSync({
   },
 });
 
-process.stdout.write(`Total files tested: ${totalFilesTested}\n`);
-process.stdout.write(`Total tests ran: ${totalTestsRan}\n`);
+process.stdout.write(`Total files tested: ${totalFilesTested}\n\n`);
+process.stdout.write(`Total tests ran: ${totalTestsPassed + totalTestsFailed}\n`);
+process.stdout.write(`Passed: ${totalTestsPassed}\n`);
+process.stdout.write(`Failed: ${totalTestsFailed}\n`);
